1f3c3081d3e401c25d3c2ba1647669ccffac3169
Karoline-art <kpol293322@edu.ucl.dk>
2021-04-16 12:07:09 +0200

    JS til spørgeskema

---
+++ quizspg.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Hvor mange børn og unge taler ikke med andre om, hvordan de har det?",
+    answer: "64 %",
+    options: [
+      "28 %",
+      "64 %",
+      "45 %",
+      "15 %"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Hvor mange børn var på krisecenter i 2018?",
+    answer: "1915",
+    options: [
+      "2176",
+      "1625",
+      "808",
+      "1915"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Hvor mange anmeldelser var der i 2019 om fysisk vold mod børn?",
+    answer: "6867",
+    options: [
+      "6867",
+      "3448",
+      "5724",
+      "7038"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Hvor mange var patienter i psykiatrisk behandling i børne-og ungdomspsykiatrien i 2018?",
+    answer: "33128",
+    options: [
+      "18899",
+      "22892",
+      "42376",
+      "33128"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Hvor mange af de 19-årige har oplevet digital mobning?",
+    answer: "15 %",
+    options: [
+      "15 %",
+      "8 %",
+      "13 %",
+      "19 %"
+    ]
+  },
+
+];
